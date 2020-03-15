6402eb6f7cff483a8e5bd6dd14675e6984548857
Kiran Jagathlal <kiranjagz@gmail.com>
2020-03-15 23:11:51 +0200

    Add project files.

---
+++ TDD.Unit.Tests/FuzzBuzzyTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDD.Examples.CodeExamples;
+
+namespace TDD.Unit.Tests
+{
+    [TestFixture]
+    class FuzzBuzzyTests
+    {
+        private FizzBuzz fizzy;
+        public FuzzBuzzyTests()
+        {
+            fizzy = new FizzBuzz();
+        }
+
+        [TestCase("Fizzz", 3)]
+        [TestCase("Fizz", 6)]
+        [TestCase("Buzz", 5)]
+        [TestCase("Buzz", 10)]
+        [TestCase("FizzBuzz", 15)]
+        [TestCase("FizzBuzz", 30)]
+        [TestCase("", 7)]
+        public void TestFizzBuzz(string expected, int number)
+        {
+            Assert.AreEqual(expected, fizzy.FizzBuzzy(number));
+        }
+    }
+}
