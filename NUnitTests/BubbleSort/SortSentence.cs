eaccc066cc5082f60393e5044445e53efe5792ed
Kiran Jagathlal <kiranjagz@gmail.com>
2020-11-03 21:44:35 +0200

    Added sort sentence.

---
+++ NUnitTests/BubbleSort/SortSentence.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy.Generators.Emitters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests.BubbleSort
+{
+    public class SortSentence
+    {
+        public SortSentence()
+        {
+
+        }
+
+        public char[] SortSentenceUsingBubbleSort(string sentence)
+        {
+            char temp;
+            string myStr = "The quick brown fox jumped over a dog!";  
+            string str = myStr.ToLower();
+            char[] charstr = str.ToCharArray();
+            for (int i = 1; i < charstr.Length; i++)
+            {
+                for (int j = 0; j < charstr.Length - 1; j++)
+                {
+                    if (charstr[j] > charstr[j + 1])
+                    {
+                        temp = charstr[j];
+                        charstr[j] = charstr[j + 1];
+                        charstr[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(charstr); 
+            Console.ReadLine();
+
+            return charstr;
+        }
+    }
+}
